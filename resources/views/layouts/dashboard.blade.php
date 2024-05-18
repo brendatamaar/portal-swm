c3dce9cba0b9db9185ba93902c0c090dd04d74b3
ICONPLN\brendatama.ramadhan <brendatama.ramadhan@iconpln.co.id>
2024-05-18 16:16:11 +0700

    integrate template

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('body')
+
+@component('components.header')
+@endcomponent
+
+@component('components.sidebar')
+@endcomponent
+
+<main id="main" class="main">
+    @yield('content')
+</main>
+
+@component('components.footer')
+@endcomponent
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth/login');
+})->middleware('guest');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
